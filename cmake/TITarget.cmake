4fe8b3d67a64a89dbb04d217a88c1919147c844e
Nicholas Mosier <nh.mosier@gmail.com>
2020-08-12 13:44:30 -0700

    added drivers; rearranged utils

--- cmake/TITarget.cmake
+++ cmake/TITarget.cmake
@@ -1,10 +1,14 @@
 function(add_8xp TARGET MAIN_SOURCE)
   cmake_parse_arguments(TI
     ""
-    ""
-    "INCLUDE_DIRS;DEFINITIONS;SOURCES;INCLUDES"
+    "PLATFORM"
+    "INCLUDE_DIRS;DEFINITIONS;SPASM_FLAGS"
     ${ARGN}
     )
+
+  if (DEFINED TI_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "add_8xp: invalid argument")
+  endif()
 
   set(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.8xp)
 
@@ -22,12 +26,26 @@ function(add_8xp TARGET MAIN_SOURCE)
     endforeach()
   endif()
 
+  if (NOT DEFINED SPASM_FLAGS)
+    set(SPASM_FLAGS)
+  endif()
+
+  if(DEFINED TI_PLATFORM)
+    if(TI_PLATFORM STREQUAL "TI83PLUS")
+      list(APPEND DEFINITIONS -DTI83PLUS=1)
+    elseif(TI_PLATFORM STREQUAL "TI84PCE")
+      list(APPEND DEFINITIONS -DTI84PCE=1)
+      list(APPEND SPASM_FLAGS -E)
+    else()
+      message(FATAL_ERROR "add_8xp: unrecognized platform '${TI_PLATFORM}'")
+    endif()
+  endif()
+
   get_filename_component(MAIN_SOURCE ${MAIN_SOURCE} ABSOLUTE)
 
   add_custom_command(OUTPUT ${OUTPUT}
-    COMMAND ${SPASM} -N -L -E ${INCLUDE_DIRS} -I ${CMAKE_CURRENT_SOURCE_DIR} ${DEFINITIONS} ${MAIN_SOURCE} ${OUTPUT}
-    COMMAND_EXPAND_LISTS
-    DEPENDS ${MAIN_SOURCE} ${SOURCES} ${INCLUDES}
+    COMMAND ${SPASM} ${SPASM_FLAGS} ${INCLUDE_DIRS} -I ${CMAKE_CURRENT_SOURCE_DIR} ${DEFINITIONS} ${MAIN_SOURCE} ${OUTPUT}
+    DEPENDS /dev/null # always remake
     )
   
   add_custom_target(${TARGET} ALL

0e8cc22a797316f29fe4fb6b91df43ee2d1566ed
Nicholas Mosier <nh.mosier@gmail.com>
2020-08-15 08:01:41 -0700

    updated with test

--- cmake/TITarget.cmake
+++ cmake/TITarget.cmake
@@ -31,6 +31,7 @@ function(add_8xp TARGET MAIN_SOURCE)
   else()
     set(SPASM_FLAGS ${TI_SPASM_FLAGS})
   endif()
+  list(APPEND SPASM_FLAGS -A) # case sensitive
 
   if(DEFINED TI_PLATFORM)
     if(TI_PLATFORM STREQUAL "TI83PLUS")

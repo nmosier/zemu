a3305c84ea820b93b7612d15ca42e713e3ebc56c
Nicholas Mosier <nh.mosier@gmail.com>
2020-08-17 10:23:09 -0700

    use directory properties in add_8xp()

--- cmake/TITarget.cmake
+++ cmake/TITarget.cmake
@@ -12,42 +12,36 @@ function(add_8xp TARGET MAIN_SOURCE)
 
   set(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.8xp)
 
-  set(INCLUDE_DIRS)
-  if (DEFINED TI_INCLUDE_DIRS)
-    foreach(INCLUDE_DIR ${TI_INCLUDE_DIRS})
-      list(APPEND INCLUDE_DIRS -I "${INCLUDE_DIR}")
-    endforeach()
-  endif()
+  get_directory_property(INCLUDE_DIRS INCLUDE_DIRECTORIES)
+  list(APPEND TI_INCLUDE_DIRS ${INCLUDE_DIRS})
+  list(TRANSFORM TI_INCLUDE_DIRS PREPEND "-I")
 
-  set(DEFINITIONS)
-  if (DEFINED TI_DEFINITIONS)
-    foreach(DEFINITION ${TI_DEFINITIONS})
-      list(APPEND DEFINITIONS -D${DEFINITION})
-    endforeach()
-  endif()
+  get_directory_property(DEFINITIONS COMPILE_DEFINITIONS)
+  list(APPEND TI_DEFINITIONS ${DEFINITIONS})
+  list(TRANSFORM TI_DEFINITIONS PREPEND "-D")
 
-  if (NOT DEFINED TI_SPASM_FLAGS)
-    set(SPASM_FLAGS)
-  else()
-    set(SPASM_FLAGS ${TI_SPASM_FLAGS})
-  endif()
-  list(APPEND SPASM_FLAGS -N) # no color
+  get_directory_property(FLAGS COMPILE_FLAGS)
+  list(APPEND TI_SPASM_FLAGS ${COMPILE_FLAGS})
+  list(APPEND TI_SPASM_FLAGS -N) # no color
 
   if(DEFINED TI_PLATFORM)
-    if(TI_PLATFORM STREQUAL "TI83PLUS")
-      list(APPEND DEFINITIONS -DTI83PLUS=1)
-    elseif(TI_PLATFORM STREQUAL "TI84PCE")
-      list(APPEND DEFINITIONS -DTI84PCE=1)
-      list(APPEND SPASM_FLAGS -E)
+    set(PLATFORM ${TI_PLATFORM})
+  endif()
+  if(DEFINED PLATFORM)
+    if(PLATFORM STREQUAL "TI83PLUS")
+      list(APPEND TI_DEFINITIONS -DTI83PLUS=1)
+    elseif(PLATFORM STREQUAL "TI84PCE")
+      list(APPEND TI_DEFINITIONS -DTI84PCE=1)
+      list(APPEND TI_SPASM_FLAGS -E)
     else()
-      message(FATAL_ERROR "add_8xp: unrecognized platform '${TI_PLATFORM}'")
+      message(FATAL_ERROR "add_8xp: unrecognized platform '${PLATFORM}'")
     endif()
   endif()
 
   get_filename_component(MAIN_SOURCE ${MAIN_SOURCE} ABSOLUTE)
 
   add_custom_command(OUTPUT ${OUTPUT}
-    COMMAND ${SPASM} ${SPASM_FLAGS} ${INCLUDE_DIRS} -I ${CMAKE_CURRENT_SOURCE_DIR} ${DEFINITIONS} ${MAIN_SOURCE} ${OUTPUT}
+    COMMAND ${SPASM} ${TI_SPASM_FLAGS} ${TI_INCLUDE_DIRS} -I ${CMAKE_CURRENT_SOURCE_DIR} ${TI_DEFINITIONS} ${MAIN_SOURCE} ${OUTPUT}
     DEPENDS /dev/null # always remake
     )
   
---
+++ cmake/ZEMUTest.cmake
@@ -0,0 +1,47 @@
+# Test Generator
+function(add_zemu_test NAME EXEC)
+  cmake_parse_arguments(ZT
+    "JSON"
+    "VARDIR;KEY_DELAY"
+    "COMMANDS"
+    ${ARGN}
+    )
+
+  if(NOT ${ZT_JSON})
+    if(DEFINED ZT_UNPARSED_ARGUMENTS)
+      message(FATAL_ERROR "add_zemu_test: invalid arguments: ${ZT_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT DEFINED ZT_VARDIR)
+      message(FATAL_ERROR "add_zemu_test: VARDIR parameter required")
+    endif()
+
+    if(DEFINED ZT_KEY_DELAY)
+      set(ZT_KEY_DELAY -w ${ZT_KEY_DELAY})
+    endif()
+
+    # Quote Commands (???)
+    set(COMMANDS)
+    foreach(COMMAND ${ZT_COMMANDS})
+      list(APPEND COMMANDS "'${COMMAND}'")
+    endforeach()
+
+    # Generate test command
+    add_custom_command(OUTPUT ${NAME}.json
+      COMMAND ${TESTGEN} -r ../misc/ce.rom -t ZEMU -v ${ZT_VARDIR} -x ${EXEC} -o ${NAME}.json -k $<TARGET_FILE:str2keys> ${COMMANDS}
+      DEPENDS ${TESTGEN} str2keys
+      )
+    
+    # Generate test target
+    add_custom_target(${NAME}_tgt ALL
+      DEPENDS ${NAME}.json
+      )
+    
+  endif()
+  
+  # Add CTest
+  add_test(NAME ${NAME}
+    COMMAND cemu-autotester ./${NAME}.json
+    )
+
+endfunction()
